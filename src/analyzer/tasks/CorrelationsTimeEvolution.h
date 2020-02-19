4bb584ebc9c0ee0b551e2c13f694e924cac2a61c
Piotr Kubala <pkua.log@gmail.com>
2020-02-19 20:11:24 +0100

    Memory fix

--- src/analyzer/tasks/CorrelationsTimeEvolution.h
+++ src/analyzer/tasks/CorrelationsTimeEvolution.h
@@ -16,34 +16,28 @@ public:
     };
 
 private:
-    struct SymmetricMatrix {
+    class SymmetricMatrix {
+    private:
         std::size_t size_{};
         std::vector<double> elements;
 
+        [[nodiscard]] std::size_t idx(std::size_t i, std::size_t j) const {
+            Expects(i < this->size_);
+            Expects(j < this->size_);
+
+            std::size_t smaller = std::min(i, j);
+            std::size_t bigger = std::max(i, j);
+
+            return bigger*(bigger + 1)/2 + smaller;
+        }
+
+    public:
         SymmetricMatrix() = default;
         explicit SymmetricMatrix(std::size_t size_) : size_{size_}, elements(size_*(size_ + 1)/2) { }
 
         [[nodiscard]] std::size_t size() const { return this->size_; }
-
-        [[nodiscard]] double &operator()(std::size_t i, std::size_t j) {
-            Expects(i < this->size_);
-            Expects(j < this->size_);
-
-            std::size_t smaller = std::min(i, j);
-            std::size_t bigger = std::max(i, j);
-
-            return this->elements[smaller * this->size_ + bigger];
-        }
-
-        [[nodiscard]] double operator()(std::size_t i, std::size_t j) const {
-            Expects(i < this->size_);
-            Expects(j < this->size_);
-
-            std::size_t smaller = std::min(i, j);
-            std::size_t bigger = std::max(i, j);
-
-            return this->elements[smaller * this->size_ + bigger];
-        }
+        [[nodiscard]] double &operator()(std::size_t i, std::size_t j) { return this->elements[this->idx(i, j)]; }
+        [[nodiscard]] double operator()(std::size_t i, std::size_t j) const { return this->elements[this->idx(i, j)]; }
     };
 
     struct Observables {
